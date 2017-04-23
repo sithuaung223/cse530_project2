3c1dcc7af20acb3aab69d7a482bc3b7f2802b1f8
pradipforever <pradip@MP02973KH.local>
2017-04-23 16:16:21 -0500

    fixing the code for lab - part b invalid data write

--- databaseSimulation.py
+++ databaseSimulation.py
@@ -13,32 +13,54 @@ Scenario:
 """
 import random
 import simpy
+import time
 
 #test
 RANDOM_SEED = 42
 
 # Constants defined for database transaction simulation
 INTERVAL_TRANSACTION = 0.0001             # Generate new transactions roughly every x seconds
-NUM_READ_REQUESTS = 2000                # Number of read requests
-NUM_WRITE_REQUESTS = 50                 # Number of write requests
+NUM_READ_REQUESTS = 20                # Number of read requests
+NUM_WRITE_REQUESTS = 5                 # Number of write requests
 SIM_TIME = 50000                        # Simulation time in minutes
 NUM_DATA_BLOCKS = 100                   # number of data blocks
 LONGEST_READ = 5
 LONGEST_WRITE = 10
 ROLLBACK_TIME = 3
+last_read_write_time = 0
 
-def source(env, number, interval, readCounter, writeCounter):
+def source(env, writeNumber, readNumber, interval, readCounter, writeCounter):
     """Source generates write transactions randomly"""
-    for i in range(number):
-        c = invalidDirtyWrite(env, 'WriteTransaction%02d' % i, readCounter, writeCounter)
-        env.process(c)
+    for i in range(writeNumber):
+        c1 = invalidDirtyWrite(env, 'WriteTransaction%02d' % i, writeCounter)
+        c2 = read(env, 'ReadTransaction%02d' % i, readCounter)
+        env.process(c1)
+        env.process(c2)
         t = random.expovariate(1.0 / interval)
         yield env.timeout(t)
 
-def invalidDirtyWrite(env, name, readCounter, writeCounter):
-    """Write transaction in one of data blocks, check by time stamp"""
+def read(env, name, readCounter):
+    """Read transaction in one of data blocks, check by time stamp"""
     start = env.now
-    last_read_write_time = 0
+    print('%7.4f %s: Here I am reading the request' % (start, name))
+
+    with readCounter.request() as req:
+        # time takes to write the request, varies
+        patience = random.uniform(0, LONGEST_READ)
+        # write the request
+        yield env.timeout(patience)
+        # total time taken to write the request
+        end = env.now - start
+        last_read_write_time = int(round(time.time() * 1000))
+        print('last_read_write_time')
+        print (last_read_write_time)
+
+
+def invalidDirtyWrite(env, name, writeCounter):
+    """Write transaction in one of data blocks, check by time stamp"""
+    global last_read_write_time
+    current_time = int(round(time.time() * 1000))
+    start = env.now
     print('%7.4f %s: Here I am writing the request' % (start, name))
 
     with writeCounter.request() as req:
@@ -48,8 +70,10 @@ def invalidDirtyWrite(env, name, readCounter, writeCounter):
         yield env.timeout(patience)
         # total time taken to write the request
         end = env.now - start
+        print('current_time')
+        print(current_time)
 
-        if last_read_write_time > start:
+        if last_read_write_time >= current_time:
         # re-attempt the write transaction
             print('the write is invalidated, reattempting')
             # rollback the transaction, start the transaction again
@@ -62,6 +86,7 @@ def invalidDirtyWrite(env, name, readCounter, writeCounter):
             yield env.timeout(patience)
             # total time taken to write the request
             end = env.now - start
+            last_read_write_time = int(round(time.time() * 1000))
 
 # Setup and start the database transaction simulation
 print('DataBase Transaction')
@@ -71,6 +96,6 @@ env = simpy.Environment()
 # Start both read and write processes, and run
 readCounter = simpy.Resource(env, capacity=1000000000)
 writeCounter = simpy.Resource(env, capacity=1000)
-env.process(source(env, NUM_WRITE_REQUESTS, INTERVAL_TRANSACTION, readCounter, writeCounter))
+env.process(source(env, NUM_WRITE_REQUESTS, NUM_READ_REQUESTS, INTERVAL_TRANSACTION, readCounter, writeCounter))
 env.run()
 #env.run(until = 100) #total simulation time is set to 100 time unit
